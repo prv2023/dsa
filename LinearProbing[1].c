45328370a8d4c70ed8204c8bfb3729446a540975
RiyaaChavan <141586740+RiyaaChavan@users.noreply.github.com>
2023-12-13 23:44:45 +0530

    Update LinearProbing[1].c

--- LinearProbing[1].c
+++ LinearProbing[1].c
@@ -1,35 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
-int hash(int arr[], int temp){
-    int i=0;
-    while(arr[(temp+i*i)%10]!=-1){
+#include<stdio.h>
+#include<stdlib.h>
+
+int hash(int arr[], int temp) {
+    int i = 0;
+    while (arr[(temp + i * i) % 10] != -1) {
         i++;
+        if (i == 10) {
+            // Break if the hash table is full
+            return -1;
+        }
     }
-    return (temp+i*i)%10;
+    return (temp + i * i) % 10;
 }
-void main(){
-    int hashlist[10];
-    int choice=1,values=0,temp;
-    for(int i=0;i<10;i++){
-        hashlist[i]=-1;
+
+int main() {
+    int hashlist[10], i = 0, choice = 1, values = 0;
+    int temp;
+    
+    for (i = 0; i < 10; i++) {
+        hashlist[i] = -1;
     }
-    while(choice==1){
-        printf("Enter value: ");
-        scanf("%d",&temp);
-        hashlist[hash(hashlist,temp)]=temp;
-        values++;
-        if(values>9){
-            printf("The table is full: ");
+
+    while (choice == 1) {
+        printf("\nEnter values: \t");
+        scanf("%d", &temp);
+
+        int index = hash(hashlist, temp);
+        printf("Storing at index: %d",index);
+
+        if (index == -1) {
+            printf("\nHash table is full");
             break;
         }
-        printf("Continue? 1/0\n");
-        scanf("%d",&choice);
+
+        hashlist[index] = temp;
+        values++;
+
+        if (values > 9) {
+            printf("\nHash table is full");
+            break;
+        }
+
+        printf("\nContinue? (1/0): ");
+        scanf("%d", &choice);
+    }
+int index;
+    printf("\nHash table contents: ");
+    for (i = 0; i < 10; i++) {
+        printf("%d ", hashlist[i]);
     }
 
-     for(int i=0;i<10;i++){
-       printf("%d\t", hashlist[i]);
-    }
-    
+    return 0;
 }
-
-
